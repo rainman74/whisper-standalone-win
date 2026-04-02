abf05b69c92ea76c167967139cfa322d59357975
Rainman74 <rainman74@gmx.net>
2026-04-02 19:22:50 +0200

    First release

--- .archive.cmd
+++ .archive.cmd
@@ -1,1 +1,5 @@
-@tar -a -c -f whisper-standalone-win-binaries.zip --exclude="_models" --exclude=".git" --exclude=".gitattributes" --exclude=".gitignore" --exclude=".archive.cmd" --exclude="whisper-standalone-win-binaries.zip" *
+@echo off
+
+tar -a -c -f whisper-standalone-win.zip --exclude="_models" --exclude=".git" --exclude=".gitattributes" --exclude=".gitignore" --exclude=".archive.cmd" --exclude="whisper-standalone-win.zip" --exclude="whisper-nvidia-dll.zip" --exclude="cu*.dll" --exclude="nv*.dll" *
+
+tar -a -c -f whisper-nvidia-dll.zip _xxl_data\torch\lib\cu*.dll _xxl_data\torch\lib\nv*.dll
